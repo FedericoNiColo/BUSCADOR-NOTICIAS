2bc78b746a29ac380c80adf045a13e94453ceff7
Federico Ni Coló <federiconicolo@hotmail.com>
2022-10-27 10:39:01 -0300

    proyecto buscador de noticias

---
+++ src/components/ListadoNoticias.jsx
@@ -0,0 +1,39 @@
+import { Grid, Typography } from '@mui/material'
+import useNoticias from '../hooks/useNoticias'
+import Noticia from './Noticia'
+
+
+const ListadoNoticias = () => {
+
+    const { noticias } = useNoticias()
+
+
+    return (
+        <>
+            <Typography
+                textAlign={'center'}
+                marginY={5}
+                variant='h3'
+                component={'h2'}
+            >
+                Últimas Noticias
+            </Typography>
+
+            <Grid
+                container
+                spacing={2}
+            >
+                {noticias.map(noticia => (
+                    <Noticia
+                        key={noticia.url}
+                        noticia={noticia}
+                    />
+                ))}
+            </Grid>
+
+
+        </>
+    )
+}
+
+export default ListadoNoticias
